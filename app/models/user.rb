6fcbcb1d4e8320a7d5bc76448b188615369063ff
Carolina <carolina@carolinas-air.mynet>
2019-06-19 23:53:23 +0100

    Add sign up, login and log out functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
-    has_many :recipes
-    has_many :comments
-    has_many :ratings
+  has_many :recipes
+  has_many :comments
+  has_many :ratings
 
+  validates :name, uniqueness: true, presence: true
+  validates :email, presence: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  
+  has_secure_password
+
+  def self.create_by_google_pmniauth(auth)
+    self.find_or_create_by(name: auth[:info][:email]) do |u|
+        u.password = SecureRandom.hex
+    end
+  end
+
+  def self.create_by_github_omniauth(auth)
+    self.find_or_create_by(name: auth[:info][:email]) do |u|
+        u.password = SecureRandom.hex
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  resources :users
+
+  resources :categories do
+    resources :recipes
+  end
+
+  resources :recipes do
+    resources :ingredients
+  end
 end
