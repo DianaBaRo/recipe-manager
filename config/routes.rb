fd5007730a1b4a170d012a91dcd8544bf1e178e6
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-06-28 21:50:33 +0100

    Add routes for ratings, recipe/comments, comments and categories/recipes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,24 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
 
+  resources :users, only: [:new, :create, :show]
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-
-  resources :users
+  delete '/logout' => 'sessions#destroy'
 
   resources :categories do
-    resources :recipes
+    resources :recipes, only: [:index, :show]
   end
 
+  resources :comments
+
   resources :recipes do
-    resources :ingredients
+    resources :comments, only: [:new, :index]
   end
+
+  resources :ratings, only: [:new, :create]
+  
 end
