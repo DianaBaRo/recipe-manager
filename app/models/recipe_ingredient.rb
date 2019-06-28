dbda7301d8037c587e75401e4619cc8856ab02df
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-06-28 21:41:21 +0100

    Add validations for quantity and ingredient

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,8 @@
 class RecipeIngredient < ApplicationRecord
     belongs_to :recipe
     belongs_to :ingredient
+
+    validates :quantity, numericality: { only_integer: true }
+    validates :ingredient_id, uniqueness: { scope: :recipe_id, message: "should have one type of ingredient per recipe"}
+
 end
