fcdbe2c5c81ed27e739e563e039da61dffdf6b1e
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-06-28 21:37:02 +0100

    Add validation for name

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ApplicationRecord
     has_many :recipe_ingredients
     has_many :recipes, through: :recipe_ingredients
+
+    validates :name, uniqueness: true, presence: true
 end
