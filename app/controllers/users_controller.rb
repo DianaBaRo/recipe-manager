667d016fe80e927209336e70b14d03e4174a52dc
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-06-28 21:35:18 +0100

    Use @user.save not @user.valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   before_action :require_login, except: [:new, :create]
-
+  
   def new
     @user = User.new
   end
 
   def create
     @user = User.create(user_params)
-    if @user.valid?
+    if @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(:id => params[:id])
+    @user = User.find_by_id(params[:id])
   end
 
   private
