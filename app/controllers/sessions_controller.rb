8dfccf7ac32b3c24181a5d8eebd4a30d0195ac2b
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-06-28 21:34:28 +0100

    Change user to @user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(:email => params[:user][:email])
-    if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    @user = User.find_by(:email => params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       render :new
     end
