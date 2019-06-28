2a7cd0d0a930a85c464939467daafa1d286e6851
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-06-28 21:36:35 +0100

    Add validation for content and class methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :recipe
+  belongs_to :user
+  belongs_to :recipe
+
+  validates :content, presence: true
+
+  def self.by_user(user_id)
+    where(user: user_id)
+  end
+
+  def self.by_recipe(recipe_id)
+    where(recipe_id: recipe_id)
+  end 
 end
