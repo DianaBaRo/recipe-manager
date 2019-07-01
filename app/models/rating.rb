b61780fa7db733b961e9d0cf56fb9934dc9c7683
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-07-01 21:45:43 +0100

    Add range for score validation

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,6 @@ class Rating < ApplicationRecord
     belongs_to :user
     belongs_to :recipe
 
-    validates :score, numericality: { only_integer: true, less_than_or_equal_to: 5 }
+    validates :score, numericality: { only_integer: true, :in => 1..5 }
     validates :user_id, uniqueness: { scope: :recipe_id, message: "you can make one rating per recipe"}
 end
