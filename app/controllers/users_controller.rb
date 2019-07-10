afce41249189493c47efaeb901f58ab3d18c7918
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-07-10 11:49:20 +0100

    Add authorization to see personal profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
+    if current_user.id == params[:id].to_i
+      @user
+    else
+      flash[:error] = "Sorry, you are authorized to access only your profile page"
+      redirect_to user_path(current_user)
+    end
   end
 
   private
