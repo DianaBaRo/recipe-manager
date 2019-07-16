6fb58ba7b60b27e1afcbfc2eea3eae6aa0f0246c
Carolina <carolina@carolinas-air.mynet>
2019-07-16 17:17:17 +0100

    Add functionality to create categories and newest scope method in recipe

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,32 @@
 class CategoriesController < ApplicationController
   before_action :require_login
+  before_action :admin_only, except: [:index, :show]
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    if @category.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+  
   def index
     @categories = Category.all
   end
 
   def show
     @category = Category.find_by_id(params[:id])
+    @recipes = @category.recipes
   end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,25 +1,34 @@
 class Recipe < ApplicationRecord
-    belongs_to :user
-    has_many :recipe_ingredients
-    has_many :ingredients, through: :recipe_ingredients
-    
-    has_many :recipe_categories
-    has_many :categories, through: :recipe_categories
-    
-    has_many :comments
-    has_many :ratings
+  belongs_to :user
 
-    validates :name, presence: true
-    validates :cooking_time, numericality: { only_integer: true }
-    validates :servings, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 20, message: "can't be blank and has to be a number between 1 and 20" }
-    validates :directions, presence: true
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+    
+  has_many :recipe_categories
+  has_many :categories, through: :recipe_categories
+    
+  has_many :comments
+  has_many :ratings
 
-    def self.by_name(name)
-      where(['name LIKE ?', "%#{name}%"])
-    end
+  validates :name, presence: true
+  validates :cooking_time, numericality: { only_integer: true }
+  validates :servings, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 20, message: "can't be blank and has to be a number between 1 and 20" }
+  validates :directions, presence: true
+
+  accepts_nested_attributes_for :categories
+
+  scope :newest, -> { order('created_at DESC') }
+
+  def self.latest
+    self.newest.first
+  end
+
+  def self.by_name(name)
+    where(['name LIKE ?', "%#{name}%"])
+  end
   
-    def self.by_user(user_id)
-      where(user: user_id)
-    end
+  def self.by_user(user_id)
+    where(user: user_id)
+  end
 
 end
