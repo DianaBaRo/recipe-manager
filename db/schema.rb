f94f6d76f420188635879b097a1d218d757f7ef8
Carolina <carolina@carolinas-air.mynet>
2019-07-16 17:06:01 +0100

    Add provider attribute to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_124426) do
+ActiveRecord::Schema.define(version: 2019_07_16_132517) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_07_04_124426) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uid"
+    t.string "provider"
   end
 
 end
