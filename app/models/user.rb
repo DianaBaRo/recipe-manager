548290c7388f091d51ad6b244a66d67586a08a00
Carolina <carolina@carolinas-air.mynet>
2019-07-16 17:18:07 +0100

    Update class method to find or create user that login via omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,10 @@ class User < ApplicationRecord
   has_secure_password
 
   def self.find_or_create_by_omniauth(auth_hash)
-    self.where(uid: auth_hash['uid']).first_or_create do |user|
+    self.where(name: auth_hash['info']['name']).first_or_create do |user|
+      user.email = auth_hash['info']['email']
+      user.provider = auth_hash['provider']
+      user.uid = auth_hash['uid']
       user.password = SecureRandom.hex
     end
   end
